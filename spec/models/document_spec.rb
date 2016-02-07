a657425f4a0d5f5f13699814a2454b47d4b15167
Mike Martin <mike@unsupported.me>
2016-02-07 16:50:14 -0600

    Adds specs for document validations

--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  it 'is valid when created via the factory' do
+  it 'belongs to a valid user' do
     document = build(:document)
-    expect(document).to be_valid
+
+    expect(document.user).to be_valid
+  end
+
+  it 'has a public boolean set' do
+    document = build(:document)
+
+    expect(document.public).not_to be nil
+  end
+
+  it 'has a name that does not exceed 255 characters' do
+    document = build(:document)
+    name = document.name
+
+    expect(name).to satisfy { |name| name.length <= 255 }
+  end
+
+  it 'has a description that does not exceed 2000 words' do
+    document = build(:document)
+    words = document.description.split(/\s+/)
+    
+    expect(words).to satisfy { |words| words.length <= 2000 }
+  end
+
+  it 'has an attached document file' do
+    document = build(:document)
+    attached_document = document.document
+
+    expect(attached_document).to satisfy { |doc| doc.instance_of? Paperclip::Attachment }
   end
 end
