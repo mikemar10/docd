467efcc1e96cceaa722a9f5ef511bf15c1dad362
Mike Martin <mike@unsupported.me>
2016-02-08 01:38:27 -0600

    Moves document shelf into partial, adds front page popular tag display

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,16 +1,19 @@
 class DashboardController < ApplicationController
+  before_action :require_login
+
   def index
+    most_used_tags = ActsAsTaggableOn::Tag.most_used(5)
+    @shelves = most_used_tags.map { |tag| [ tag.name, filter_private(Document.tagged_with(tag.name)) ] }
   end
 
   def search
     unfiltered_results = multisearch(params[:search])
     @documents = filter_private(unfiltered_results)
-    render 'documents/index'
   end
 
   private
 
-  def filter_private(search_results)
-    search_results.reject { |result| result.private? && result.user != current_user }
+  def filter_private(records)
+    records.reject { |result| result.private? && result.user != current_user }
   end
 end
