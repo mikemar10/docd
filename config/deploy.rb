9339b120ac9a7445b76277eedc841f688c3aacd7
Mike Martin <mike@unsupported.me>
2016-02-08 23:54:55 -0600

    Capistrano updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,8 @@ set :keep_releases, 5
 # Default value for :linked_files is []
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml', 
-  'config/secrets.yml'
+  'config/secrets.yml',
+  'config/unicorn.rb'
 )
 
 # Default value for linked_dirs is []
